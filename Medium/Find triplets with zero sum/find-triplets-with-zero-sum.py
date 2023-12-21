04355c99323b5a96ef38a60cb7f4cc37ef862118
chetangarg2101 <63465165+chetangarg2101@users.noreply.github.com>
2023-12-21 23:32:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find triplets with zero sum/find-triplets-with-zero-sum.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    #Function to find triplets with zero sum.    
+    def findTriplets(self, arr, n):
+        #code here
+        arr.sort()
+        for i in range(n):
+            l=i+1
+            r=n-1
+            remain=-arr[i]
+            while l<r:
+                if arr[l]+arr[r]<remain:
+                    l+=1
+                elif arr[l]+arr[r]>remain:
+                    r-=1
+                else:
+                    return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().strip().split()))
+        if(Solution().findTriplets(a,n)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
