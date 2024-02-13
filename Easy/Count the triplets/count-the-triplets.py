f7341f012d37b614df6dbe2637483584e813593e
chetangarg2101 <63465165+chetangarg2101@users.noreply.github.com>
2024-02-13 19:37:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the triplets/count-the-triplets.py
@@ -0,0 +1,26 @@
+# User function Template for python3
+class Solution:
+	def countTriplet(self, arr, n):
+		# code here
+		count = 0
+        b = set(arr)
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+               if(arr[i]+arr[j] in b):
+                 count+=1
+        return count
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		arr = [int(x) for x in input().split()]
+
+		ob = Solution()
+		ans = ob.countTriplet(arr, n)
+		print(ans)
+
+# } Driver Code Ends
