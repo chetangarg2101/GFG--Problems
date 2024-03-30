3b7c55ddfe5f3d6f3eaa695f1f494d668b8e1822
chetangarg2101 <63465165+chetangarg2101@users.noreply.github.com>
2024-03-30 22:18:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rearrange Array Alternately/rearrange-array-alternately.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+class Solution:
+    ##Complete this function
+    #Function to rearrange  the array elements alternately.
+    def rearrange(self,arr, n): 
+        ##Your code here
+        temp = n*[None]
+        low, high = 0, n-1
+        flag = True
+        for i in range(n):
+            if flag is True:
+                temp[i] = arr[high]
+                high -= 1
+            else:
+                temp[i] = arr[low]
+                low += 1
+            flag = bool(1-flag)
+        for i in range(n):
+            arr[i] = temp[i]
+        return arr
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            ob.rearrange(arr,n)
+            
+            for i in arr:
+                print(i,end=" ")
+            
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
