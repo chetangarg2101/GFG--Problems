79a1efd0ca48b28362fae5415114ca6c68d789ef
Chetan Garg <63465165+chetangarg2101@users.noreply.github.com>
2024-04-30 21:14:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximize The Cut Segments/maximize-the-cut-segments.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to find the maximum number of cuts.
+    def maximizeTheCuts(self,n,x,y,z):
+        #code here
+        dp=[0]*(n+1)
+        dp[0]=1
+        for i in range(1,n+1):
+            a=0
+            b=0
+            c=0
+            if x<=i:
+                a=dp[i-x]
+            if y<=i:
+                b=dp[i-y]
+            if z<=i:
+                c=dp[i-z]
+            if (a==0 and b==0 and c==0):
+                dp[i]=0
+            else:
+                dp[i]=max(1+a,1+b,1+c)
+        if (dp[n]==0):
+            return 0
+        return dp[n]-1
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        x,y,z=[int(x) for x in input().split()]
+        
+        print(Solution().maximizeTheCuts(n,x,y,z))
+# } Driver Code Ends
