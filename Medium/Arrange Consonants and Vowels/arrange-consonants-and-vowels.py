270ba3f6f0e7a1b46d116b5d9bf649dcbe0b8413
Chetan Garg <63465165+chetangarg2101@users.noreply.github.com>
2024-05-01 11:38:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arrange Consonants and Vowels/arrange-consonants-and-vowels.py
@@ -0,0 +1,84 @@
+#User function Template for python3
+
+"""
+# Node Class
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    #Function to reverse a linked list.
+    def arrangeCV(self, head):
+        # Code here
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        current=head
+        v=[]
+        c=[]
+        n=0
+        while current:
+            if current.data in vowels:
+                v.append(current.data)
+            else:
+                c.append(current.data)
+            n+=1
+            current=current.next
+
+        l=Linked_List()
+        for i in v:
+            l.insert(i)
+        for i in c:
+            l.insert(i)
+        return l.head
+
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class Node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+
+def printList(head):
+    tmp = head
+    while tmp:
+        print(tmp.data, end=' ')
+        tmp = tmp.next
+    print()
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        n = int(input())
+        arr = [str(x) for x in input().split()]
+
+        lis = Linked_List()
+        for i in arr:
+            lis.insert(i)
+
+        newHead = Solution().arrangeCV(lis.head)
+        printList(newHead)
+
+# } Driver Code Ends
