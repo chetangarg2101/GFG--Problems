85017c88797e240fd0f824a4befe9f6f21884a7c
Chetan Garg <63465165+chetangarg2101@users.noreply.github.com>
2024-06-01 20:59:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Odd Even Problem/odd-even-problem.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    def oddEven(self, s : str) -> str:
+        # code here
+        even="bdfhjlnprtvxz"
+        ans=0
+        ss=set(s)
+        for i in ss:
+            if i in even:
+                if s.count(i)%2==0:
+                    ans+=1
+            else:
+                if s.count(i)%2!=0:
+                    ans+=1
+        if ans%2==0:
+            return "EVEN"
+        return "ODD"
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        s = (input())
+
+        obj = Solution()
+        res = obj.oddEven(s)
+
+        print(res)
+
+# } Driver Code Ends
