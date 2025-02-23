833374159d1601cc2c63be337fee6e342b0b116a
Chetan Garg <63465165+chetangarg2101@users.noreply.github.com>
2025-02-23 22:47:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.py
@@ -0,0 +1,36 @@
+# User function Template for python3
+
+class Solution:
+    # Function to find the next greater element for each element of the array.
+    def nextLargerElement(self, arr):
+        # code here
+        n=len(arr)
+        ans=[None]*n
+        stack=[]
+        for i in range(n-1,-1,-1):
+            while stack and stack[-1]<=arr[i]:
+                stack.pop()
+            if not stack:
+                ans[i]=-1
+            else:
+                ans[i]=stack[-1]
+            stack.append(arr[i])
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+t = int(input())  # number of test cases
+for _ in range(t):
+    arr = list(map(int, input().split()))  # input array
+    s = Solution().nextLargerElement(arr)  # find the next greater elements
+
+    # Output formatting
+    if s:
+        print(" ".join(map(str, s)))  # Print next greater elements
+    else:
+        print("[]")  # Print empty list if no next greater element is found
+    print("~")
+# } Driver Code Ends
